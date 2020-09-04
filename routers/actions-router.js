8cf531c3bec3cee3030d4153560d96e0f3e529ae
Stephen Gary <sgary0@protonmail.com>
2020-09-04 11:37:10 -0400

    interview questions, index.js router set up

---
+++ routers/actions-router.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+const db = require("../data/helpers/actionModel.js");
+const dbp = require("../data/helpers/projectModel.js");
+
+module.exports = router;
