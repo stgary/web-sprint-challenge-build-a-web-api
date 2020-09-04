4f5d8556110c160c17d5ccbae4fa7413869692d0
Stephen Gary <sgary0@protonmail.com>
2020-09-04 11:47:43 -0400

    middleware

--- routers/actions-router.js
+++ routers/actions-router.js
@@ -4,4 +4,40 @@ const router = express.Router();
 const db = require("../data/helpers/actionModel.js");
 const dbp = require("../data/helpers/projectModel.js");
 
+// custom middleware
+
+function validateAction(req, res, next) {
+    const { notes, description } = req.body
+    if (notes === undefined) {
+       res.status(400).json({
+          message: "Missing action note (MW)."
+      });
+    }
+    if (description === undefined) {
+       res.status(400).json({
+          message: "Missing action description (MW)."
+      });
+    }
+    next();
+ }
+ 
+ function validateProjectId(req, res, next) {
+    dbp.get(req.params.id)
+       .then(dbRes => {
+          if (dbRes) {
+             next();
+          } else {
+             res.status(400).json({ 
+                message: "Invalid project id (MW)."
+             }); 
+          }
+       })
+       .catch(error => {
+          console.log(error);
+             res.status(500).json({
+                message: "Failure validating project id (MW)."
+             });
+       });
+ }
+
 module.exports = router;
--- routers/projects-router.js
+++ routers/projects-router.js
@@ -3,4 +3,23 @@ const router = express.Router();
 
 const db = require("../data/helpers/projectModel.js");
 
+// CRUD operations
+
+// custom middleware
+
+function validateProject(req, res, next) {
+    const { name, description } = req.body
+    if (name === undefined) {
+      res.status(400).json({
+        message: "Missing project name (MW)."
+      });
+    }
+    if (description === undefined) {
+      res.status(400).json({
+        message: "Missing project description (MW)."
+      });
+    }
+    next();
+ }
+
 module.exports = router;
